982ca4eec48840699944ef07479b2170b6c4dad3
DomePuma <mathys.mechain@gmail.com>
2024-11-06 13:58:35 +0100

    Exo finished
    Forgot to commit before

--- chapter-0/src/main.cpp
+++ chapter-0/src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cmath>
+
+#include "Point2D.hpp"
+
+void DisplayPoint2D(Point2D &point2D) {
+    std::cout << point2D << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Point2D p1(2, 2);
+
+    std::cout << p1.Rho() << std::endl;
+    std::cout << p1.Theta() * 180 / M_PI << std::endl;
+
+    DisplayPoint2D(p1);
+
+    p1.Rotate(M_PI/4);
+
+    std::cout << p1.Rho() << std::endl;
+    std::cout << p1.Theta() * 180 / M_PI << std::endl;
+
+    DisplayPoint2D(p1);
+
+    Point2D p2(4, 3);
+
+    std::cout << p2.allPointNumber << std::endl;
+    std::cout << p1.GetNumber() << std::endl;
+
     return 0;
 }
